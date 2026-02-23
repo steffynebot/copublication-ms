96afc97023e87ea0ea4fd03f8785e8eb67f672fd
SteffyAndreaNebot <105048066+steffynebot@users.noreply.github.com>
2026-02-23 12:32:54 +0000

    maj logo

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import networkx as nx
 # -------------------
 # Page config
 # -------------------
-st.set_page_config(page_title="Copublications Inria", layout="wide")
+st.set_page_config(page_title="Copublications MS_Inria", layout="wide")
 
 # -------------------
 # Détection du thème actuel
